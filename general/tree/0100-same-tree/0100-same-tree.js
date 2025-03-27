21e34bc95d568c0ee3ef387cd11bd0c0ac4044d9
Abhishek Kumar Verma <akv.abd@gmail.com>
2025-03-27 17:45:45 +0530

    Uploaded code

--- general/tree/0100-same-tree/0100-same-tree.js
+++ general/tree/0100-same-tree/0100-same-tree.js
@@ -11,20 +11,36 @@
  * @param {TreeNode} q
  * @return {boolean}
  */
-var isSameTree = function(p, q) {
-    function treeMatch(root1, root2){   // this method is not required, just created while trying to rename params.
-        if(!root1 && !root2){
-            return true;
-        }
-        if((!root1 && root2) || (root1 && !root2)) {
-            return false;
-        }
+// Using recursion
+// var isSameTree = function(p, q) {
+//     function isSameTree(root1, root2){   // this method is not required, just created while trying to rename params.
+//         if(!root1 && !root2){
+//             return true;
+//         }
+//         if((!root1 && root2) || (root1 && !root2)) {
+//             return false;
+//         }
 
-        if(root1.val !== root2.val){
-            return false;
-        }
+//         if(root1.val !== root2.val){
+//             return false;
+//         }
 
-        return treeMatch(root1.left, root2.left) && treeMatch(root1.right, root2.right);
+//         return isSameTree(root1.left, root2.left) && isSameTree(root1.right, root2.right);
+//     }
+//     return treeMatch(p,q);
+// };
+// TC - n+m, SC - max height among both trees
+
+// Using Queue
+var isSameTree = function(a,b){
+    let q = [a,b];
+    while(q.length){
+        let [a,b] = [q.shift(), q.shift()];
+        if(!a && !b) continue;
+        if(!a || !b || (a.val != b.val)) return false;
+        q.push(a.left, b.left, a.right, b.right);
     }
-    return treeMatch(p,q);
-};
+    return true;
+}
+
+// TC - n+m, SC - n+m since last level of both will be present in worst case which is n/2 & m/2
