01fbddf1366afcff2a5352c24a2ed7a9748dcf70
Abhishek Kumar Verma <akv.abd@gmail.com>
2025-03-28 09:24:18 +0530

    Uploaded code

--- general/tree/0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.js
+++ general/tree/0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.js
@@ -13,13 +13,13 @@
  * @return {TreeNode}
  */
 
-// Recursive iteration
+// Recursive solution
 // var lowestCommonAncestor = function(root, p, q) {
 //     if(!root || root == p || root == q){    // Here we can with compare values OR nodes directly.
 //         return root;
 //     }
 //     let large = Math.max(p.val, q.val), // Gotcha : Here large/small are primitive and root/p/q are nodes. So be careful while comparision.
-//         small = Math.min(p.val, q.val);
+//         small = Math.min(p.val, q.val);  // Tip : can also extract root's value to primitive
 //     if(root.val > large){   // root = 10, p = 6, q = 8 then lca in left subtree
 //         root = root.left;    // We could also make the LCA call directly here instead.
 //     } else if (root.val < small){
@@ -31,22 +31,44 @@
 // };
 
 // Iterative implementation
+// var lowestCommonAncestor = function(root, p, q) {
+//     if(!root || root == p || root == q) {
+//         return root;
+//     }
+
+//     // let q = [root], // stack also same since order does not matter. 
+//     // Not required since we directly know the next element and there is just one path.
+//     let large = Math.max(p.val, q.val), small = Math.min(p.val, q.val);
+
+//     while(root){
+//         if(root.val > large){
+//             root = root.left;
+//         } else if (root.val < small){
+//             root = root.right;
+//         } else {
+//             return root;
+//         }
+//     }
+//     // return null;
+// }
+
+// Subtree Not found Approach
 var lowestCommonAncestor = function(root, p, q) {
-    if(!root || root == p || root == q) {
+    if (!root || root === p || root === q) {  // Gotcha: p & q are nodes, not values!
         return root;
     }
-
-    // let q = [root], // stack also same since order does not matter
-    let large = Math.max(p.val, q.val), small = Math.min(p.val, q.val);
-
-    while(root){
-        if(root.val > large){
-            root = root.left;
-        } else if (root.val < small){
-            root = root.right;
-        } else {
-            return root;
-        }
+    
+    let leftNode = lowestCommonAncestor(root.left, p, q),
+        rightNode = lowestCommonAncestor(root.right, p, q);
+    
+    // If one subtree is null, return the other non-null subtree
+    if (!leftNode) {
+        return rightNode;  // if leftNode is null, the result is in the right subtree
+    } else if (!rightNode) {
+        return leftNode;   // if rightNode is null, the result is in the left subtree
+    } else {
+        // Both nodes found, current node is LCA
+        return root;
     }
-    // return null;
-}
+};
+
