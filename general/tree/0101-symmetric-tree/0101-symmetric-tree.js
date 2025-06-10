f942352b44cd306bf2237bae3f83fc2cd809bafe
Abhishek Kumar Verma <akv.abd@gmail.com>
2025-06-10 20:47:07 +0530

    Uploaded code

--- general/tree/0101-symmetric-tree/0101-symmetric-tree.js
+++ general/tree/0101-symmetric-tree/0101-symmetric-tree.js
@@ -10,27 +10,29 @@
  * @param {TreeNode} root
  * @return {boolean}
  */
-// var isSymmetric = function(root) {
-//     if(!root) return true;
-//     function helper(p,q){
-//         if(!p && !q) return true;
-//         if(!p || !q || (p.val !== q.val)) return false;
-
-//         return helper(p.left, q.right) && helper(p.right, q.left);
-//     }
-//     return helper(root.left, root.right);
-// };
-
 function isSymmetric(root){
     if(!root) return true;
 
-    let q = [[root.left, root.right]];
-    while(q.length){
-        let [root1, root2] = q.shift();
-        if(!root1 && !root2) continue;
+    function areSame(root1, root2){
+        if(!root1 && !root2) return true;
         if(!root1 || !root2 || root1.val != root2.val) return false;
 
-        q.push([root1.left, root2.right], [root1.right, root2.left]);
+        return areSame(root1.left, root2.right) && areSame(root1.right, root2.left);
     }
-    return true;
+
+    return areSame(root.left, root.right);
 }
+
+// function isSymmetric(root){
+//     if(!root) return true;
+
+//     let q = [[root.left, root.right]];
+//     while(q.length){
+//         let [root1, root2] = q.shift();
+//         if(!root1 && !root2) continue;
+//         if(!root1 || !root2 || root1.val != root2.val) return false;
+
+//         q.push([root1.left, root2.right], [root1.right, root2.left]);
+//     }
+//     return true;
+// }
