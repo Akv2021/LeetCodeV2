cd59995a1e317296e147fa1e21b36b72d56186e6
Abhishek Kumar Verma <akv.abd@gmail.com>
2025-07-02 10:28:07 +0530

    Uploaded code

--- general/tree/0572-subtree-of-another-tree/0572-subtree-of-another-tree.js
+++ general/tree/0572-subtree-of-another-tree/0572-subtree-of-another-tree.js
@@ -11,16 +11,32 @@
  * @param {TreeNode} subRoot
  * @return {boolean}
  */
-var isSubtree = function(root, subRoot) {
-    if(sameTree(root, subRoot))
-        return true;
+// var isSubtree = function(root, subRoot) {
+//     if(sameTree(root, subRoot))
+//         return true;
 
-    if(!root) return false;
-    return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
+//     if(!root) return false;
+//     return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
     
-    function sameTree(root1, root2){
-        if(!root1 && !root2) return true;
-        if(!root1 || !root2 || root1.val != root2.val) return false;
-        return sameTree(root1.left, root2.left) && sameTree(root1.right, root2.right)
-    }
-};
+//     function sameTree(root1, root2){
+//         if(!root1 && !root2) return true;
+//         if(!root1 || !root2 || root1.val != root2.val) return false;
+//         return sameTree(root1.left, root2.left) && sameTree(root1.right, root2.right)
+//     }
+// };
+
+
+var isSubtree = function(root, subRoot) {
+    let rootSerialized = serialize(root); 
+    let subRootSerialized = serialize(subRoot); 
+
+    return rootSerialized.includes(subRootSerialized);
+} 
+
+function serialize(root){
+    if(!root) return 'X';
+
+    let leftSerialized = serialize(root.left);
+    let rightSerialized = serialize(root.right);
+    return `#${root.val},${leftSerialized},${rightSerialized}`;
+}
