ddf1093b36ee3f5adeee3b0b14c366c3f978f1f6
Abhishek Kumar Verma <akv.abd@gmail.com>
2025-07-02 08:43:39 +0530

    Uploaded code

--- general/tree/0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.js
+++ general/tree/0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.js
@@ -10,24 +10,9 @@
  * @param {TreeNode} root
  * @return {number}
  */
-// var maxDepth = function(root) {
-//     if(!root) return 0;
-//     return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
-// };
-
 var maxDepth = function(root) {
     if(!root) return 0;
-    let q = [root], size = 1, lc = 0;
-    while(q.length){
-        let node = q.shift();
-        size--;
-        if(node.left) q.push(node.left);
-        if(node.right) q.push(node.right);
-
-        if(size == 0){
-            lc++;
-            size = q.length;
-        }
-    }
-    return lc;
-}
+    let leftDepth = maxDepth(root.left);
+    let rightDepth = maxDepth(root.right);
+    return Math.max(leftDepth, rightDepth) + 1;
+};
