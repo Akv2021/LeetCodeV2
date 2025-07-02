19949b9cc7b7cd429776475d9b6077dd6ef6b758
Abhishek Kumar Verma <akv.abd@gmail.com>
2025-07-02 12:28:14 +0530

    Uploaded code

--- general/tree/0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.js
+++ general/tree/0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.js
@@ -11,26 +11,51 @@
  * @return {number[][]}
  */
 // const { Queue } = require('@datastructures-js/queue');
+// var levelOrder = function(root) {
+//     // console.log(Queue.toString());
+//     if(!root) return [];
+    
+//     let result = [];
+//     let q = new Queue();
+//     q.enqueue(root);
+
+//     while(q.size()){
+//         let levelSize = q.size();
+//         let levelNodes = [];
+//         while(levelSize){
+//             let node = q.dequeue();
+//             levelSize--;
+//             levelNodes.push(node.val);
+
+//             if(node.left) q.enqueue(node.left);
+//             if(node.right) q.enqueue(node.right);
+//         }
+//         result.push(levelNodes);
+//     }
+//     return result;
+// };
+
+
 var levelOrder = function(root) {
-    // console.log(Queue.toString());
     if(!root) return [];
     
     let result = [];
-    let q = new Queue();
-    q.enqueue(root);
+    let q = [root];
+    let levelSize = 1, levelNodes = []; // Declare variables outside loop since need in all iterations.
 
-    while(q.size()){
-        let levelSize = q.size();
-        let levelNodes = [];
-        while(levelSize){
-            let node = q.dequeue();
-            levelSize--;
-            levelNodes.push(node.val);
+    while(q.length){
+        let node = q.shift();
+        levelSize--;
+        levelNodes.push(node.val);
 
-            if(node.left) q.enqueue(node.left);
-            if(node.right) q.enqueue(node.right);
+        if(node.left) q.push(node.left);
+        if(node.right) q.push(node.right);
+
+        if(!levelSize){     // On level end, update ans and reset vaiables.
+            levelSize = q.length;
+            result.push(levelNodes);
+            levelNodes = [];
         }
-        result.push(levelNodes);
     }
     return result;
 };
